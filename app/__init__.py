6ee529f39252cf2fa70877da0b1634fb371b1189
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-28 11:08:44 +0300

    adjusts for travis.

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from app.config import app_config
 import json
 
-def create_app(config_name):
+def create_app():
     from app.models.user import User
     from app.database import Database
     from app.models.question import Question
@@ -14,9 +14,6 @@ def create_app(config_name):
     )
 
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-
   
     jwt = JWTManager(app)
 
--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 import os
 from app import create_app
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
 
-config_name = os.getenv("APP_SETTINGS") # config_name = "development"
-app = create_app(config_name)
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
