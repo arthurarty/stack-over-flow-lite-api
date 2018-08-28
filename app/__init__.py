b72d44c8c4fe95e2d9b85b7b56fa37e5adaf7e11
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-28 11:48:46 +0300

    add jwt-secret Changes to be committed: 	modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app():
     )
 
     app = Flask(__name__, instance_relative_config=True)
-  
+    app.config['JWT_SECRET_KEY'] = 'qweBas12@!asBASD'
     jwt = JWTManager(app)
 
     db_conn = Database()
