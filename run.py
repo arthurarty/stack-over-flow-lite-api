fbb80e5b8e76abd62107a3fa83960d347fd543cc
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-30 11:50:56 +0300

    apidocs removed from json rule. Changes to be committed: 	modified:   run.py

--- run.py
+++ run.py
@@ -23,8 +23,11 @@ def method_not_allowed(error):
 
 @app.before_request
 def check_for_json():
-    if not request.content_type == 'application/json':
-        return jsonify({"msg": "Content type not json"})
+    if not request.content_type == 'application/json' \
+            and '/auth/' in request.path:
+        if not request.content_type == 'application/json' \
+                and '/v1/' in request.path:
+            return jsonify({"msg": "Content type not json"})
 
 if __name__ == '__main__':
     app.run(debug=True)
